de0779f19432362d5e3d12eb1f65380ad2d3d66a
estelaV9 <esteladeveg@gmail.com>
2024-10-31 19:58:14 +0100

    terminar el ejercicio 6

--- Tema2_GeneracionInterfacesUsuario/ejer6_text_column_row/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer6_text_column_row/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 /*EJERCICIO 6:  Crea una pantalla que utilice Text, Column y Row
 para organizar el contenido.
  • El Column debe alinear dos textos verticalmente, y el Row alinear dos botones
@@ -10,63 +11,72 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PaginaPrincipal(title: 'Ejercicio 6 Pagina 32'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class PaginaPrincipal extends StatefulWidget {
+  const PaginaPrincipal({super.key, required this.title});
+
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<PaginaPrincipal> createState() => _PantallaConColumna();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _PantallaConColumna extends State<PaginaPrincipal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+            const Column(
+              children: <Widget>[
+                Text("Column", style: TextStyle(fontSize: 24),),
+                Text("Texto1", style: TextStyle(fontSize: 24),),
+                Text("Texto2", style: TextStyle(fontSize: 24),),
+              ],
+            ),
+
+            // AÑADIR UN MARGEN ENTRE COLUMNA Y FILA
+            const Padding(padding: EdgeInsets.only(
+              top: 40, bottom: 40,
+            )),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                ElevatedButton( // BOTON 1
+                onPressed: (){
+                  print("has pulsado el boton 1");
+                }, child: (const Text("Button1", style: TextStyle(fontSize: 24),))),
+
+                // AÑADIR UN MARGEN ENTRE LOS BOTONES
+                const Padding(padding: EdgeInsets.only(
+                  left: 20, right: 20,
+                )),
+
+                ElevatedButton( // BOTON 2
+                    onPressed: (){
+                      print("has pulsado el boton 2");
+                    }, child: (const Text("Button2", style: TextStyle(fontSize: 24),)))
+              ],
+            )
+          ]),
+        ));
   }
 }
