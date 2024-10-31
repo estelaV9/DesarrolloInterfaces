03767fa9665e01f16708b126a3d350526d978845
estelaV9 <esteladeveg@gmail.com>
2024-10-31 22:15:05 +0100

    crear un gridview con columnas

--- Tema2_GeneracionInterfacesUsuario/ejer8_tarjetas/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer8_tarjetas/lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ejercicio 8',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -38,12 +40,50 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+            // CONTENEDOR PARA METER EL GRIDVIEW
+            child: Container(
+                width: 600.0, // ANCHO DEL GRIDVIEW
+                height: 600.0, // ALTURA DEL GRIDVIEW
+                child: GridView.count(
+                    crossAxisCount: 2,
+                    // COLUMNAS
+                    crossAxisSpacing: 10.0,
+                    // MARGEN HORIZONTAL ENTRE LAS COLUMNAS
+                    mainAxisSpacing: 10.0,
+                    // MARGEN VERTICAL ENTRE LAS COLUMNAS
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.purple,width: 4),
+                          boxShadow: const [
+                            BoxShadow(
+                              blurRadius: (10),
+                              color: Colors.purple
+                            )
+                          ]
+                        ),
+                          child: Center(
+                              child: Column(children: [
+                            const Text("TEXTO 1",
+                                style: TextStyle(fontSize: 24)),
 
-    ));
+                            const Padding(padding: EdgeInsets.all(10)),
+
+                            Image.network(
+                              "https://img.freepik.com/foto-gratis/gato-futurista-gafas-proteccion_23-2150969319.jpg?semt=ais_hybrid",
+                              width: 200,
+                              height: 200,
+                            )
+                          ]))),
+                      Container(color: Colors.blue, child: Text('2')),
+                      Container(color: Colors.green, child: Text('3')),
+                    ]))));
   }
 }
