77cd714ffe02c3f025652752c2141d7869f21981
estelaV9 <esteladeveg@gmail.com>
2024-10-31 19:04:43 +0100

    terminar el ejercicio 2

--- Tema2_GeneracionInterfacesUsuario/ejer2_texto_y_boton/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer2_texto_y_boton/lib/main.dart
@@ -12,59 +12,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Ejercicio 2 Página 31"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // CENTRA LA COLUMNA
+            children: [
+              Text("Texto 1", style: TextStyle(fontSize: 24)),
+              Text("Texto 2", style: TextStyle(fontSize: 24)),
+
+              // AÑADIR UN MARGEN ENTRE LOS TEXTOS Y EL BOTON DE 20
+              const Padding(padding: EdgeInsets.only(
+                top: 20,
+                bottom: 20,
+              ),),
+
+              ElevatedButton(
+                  onPressed: () {
+                    print("Has pulsado el boton"); // CUANDO SE PRESIONA EL BOTON SE MUESTRA UN MENSAJE POR CONSOLA
+                  },
+                  child: (Text("PULSAR", style: TextStyle(fontSize: 24)) // TEXTO DEL BOTON
+                  )
+              )
+            ],
+          ),
+        )
     );
   }
 }
