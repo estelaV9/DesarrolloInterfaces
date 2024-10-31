9f3b09510474c2c110bc369b801352d2b3577dba
estelaV9 <esteladeveg@gmail.com>
2024-10-31 21:11:24 +0100

    crear ejercicio 8

---
+++ Tema2_GeneracionInterfacesUsuario/ejer8_tarjetas/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+/* EJERCICIO 8: Crea una pantalla con varias tarjetas (Card) que
+tengan un título y una imagen.
+Aplica “padding” y “margin” a cada tarjeta, y usa “Box Decoration” para añadir
+bordes redondeados y una sombra */
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Ejercicio 8 Pagina 51'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+    ));
+  }
+}
