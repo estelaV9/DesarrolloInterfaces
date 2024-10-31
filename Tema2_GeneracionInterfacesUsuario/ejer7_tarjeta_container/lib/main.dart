2447606120ce67baa6902254dd444a846ce3d3d2
estelaV9 <esteladeveg@gmail.com>
2024-10-31 21:06:02 +0100

    terminar el ejercicio 7

--- Tema2_GeneracionInterfacesUsuario/ejer7_tarjeta_container/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer7_tarjeta_container/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+/*EJERCICIO 7:  Diseña una tarjeta dentro de un Container con un color
+blanco de fondo, bordes y relleno personalizado. Utiliza las propiedades
+de margin, padding y borderRadius de Container para realizarlo*/
 void main() {
   runApp(const MyApp());
 }
@@ -7,62 +10,56 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ejercicio 7',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PaginaPrincipal(title: 'Ejercicio 7 Pagina 32'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class PaginaPrincipal extends StatefulWidget {
+  const PaginaPrincipal({super.key, required this.title});
+
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<PaginaPrincipal> createState() => _TarjetaContainer();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _TarjetaContainer extends State<PaginaPrincipal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.all(16.0), // ESPACIO EXTERIOR ALREDEDOR DE LA TARJETA
+            color: Colors.white,
+            child: Container(
+              width: 200.0, // ANCHO DEL CONTENEDOR
+              height: 200.0, // ALTURA DEL CONTENEDOR
+              alignment: Alignment.center,
+               padding: const EdgeInsets.all(25.0), // PADDING PARA EL INTERIOR DEL CONTENEDOR
+              decoration: BoxDecoration(
+                color: Colors.white, // COLOR DE FONDO DEL CONTENEDOR INTERIOR
+                border: Border.all(
+                  color: Colors.purple, // COLOR DEL BORDE
+                  width: 2.0, // ANCHO DEL BORDE
+                ),
+                borderRadius: BorderRadius.circular(10), // RADIO PARA BORDES REDONDEADOS
+              ),
+              child: const Text("Texto dentro de un contenedor", style: TextStyle(fontSize: 24),)
+            ),
+          ),
+        ));
   }
 }
