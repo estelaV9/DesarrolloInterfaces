bd4cba4dfc38336d0d9d21343550b51eebd186e7
estelaV9 <esteladeveg@gmail.com>
2024-11-03 21:43:06 +0100

    crear contenedor con una imagen d efondo y una imagen de perfil

--- Tema2_GeneracionInterfacesUsuario/ejer9_interfaz_usuario/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer9_interfaz_usuario/lib/main.dart
@@ -15,18 +15,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ejercicio 9',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Ejercicio 9 Página 51'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -42,6 +44,38 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
+        child: Stack(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                width: 400,
+                height: 400,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center, // CENTRADO
+                    children: [
+                      Image.network(
+                          'https://images.unsplash.com/photo-1663970206579-c157cba7edda?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9uZG8lMjBkZSUyMHBhbnRhbGxhJTIwcGFyYSUyMHBjfGVufDB8fDB8fHww'),
+                    ])),
+            Positioned(
+              child: Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle, // CONTENEDOR CIRCULAR
+                  border: Border.all(
+                    color: Colors.black, // COLOR BORDE
+                    width: 2, // GROSOR DEL BORDE
+                  )
+                ),
+                child: CircleAvatar(
+                  radius: 100, // Ajusta el radio para que coincida con el tamaño deseado
+                  backgroundColor: Colors.black, // El color de fondo del círculo
+                  backgroundImage: NetworkImage('https://i.pinimg.com/236x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'), // Imagen de fondo
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
