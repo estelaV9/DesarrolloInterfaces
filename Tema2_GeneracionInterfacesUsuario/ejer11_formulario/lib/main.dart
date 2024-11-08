98a20cb979fb539fb8c73541d36bd9c86d388fb1
estelaV9 <esteladeveg@gmail.com>
2024-11-08 12:59:38 +0100

    crear los textfield y el boton

--- Tema2_GeneracionInterfacesUsuario/ejer11_formulario/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer11_formulario/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 /* EJERCICIO 11: Crea un formulario de registro que tenga TextFields para nombre, correo
 electrónico y contraseña. Usa el widget Form para gestionar la validación, y
 añade un botón para enviar el formulario */
@@ -14,9 +15,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ejercicio 11',
+      debugShowCheckedModeBanner: false, // QUITAR LA MARCA DE DEBUG
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
       home: const FormPage(title: 'Ejercicio 11: Formulario de Registro'),
     );
@@ -29,17 +30,64 @@ class FormPage extends StatefulWidget {
   final String title;
 
   @override
-  State<FormPage> createState() => _MyHomePageState();
+  State<FormPage> createState() => _RegistrationFormState();
 }
 
-class _MyHomePageState extends State<FormPage> {
+class _RegistrationFormState extends State<FormPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("")
-        ],
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Center(
+          child: Text('Formulario de Registro',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        )
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              // CAMPO NOMBRE
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Nombre'),
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 10),
+
+              // CAMPO CORREO ELECTRONICO
+              TextFormField(
+                decoration:
+                    const InputDecoration(labelText: 'Correo electrónico'),
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 10),
+
+              // CAMPO DE CONTRASEÑA
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'Contraseña'),
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 20),
+
+              // BOTON ENVIAR
+              ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Formulario Enviado')),
+                  );
+                },
+                child: const Text(
+                  'Registrar',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
