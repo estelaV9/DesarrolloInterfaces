e8fee05620b5396f3fd47c04f25542d8e29455d4
estelaV9 <esteladeveg@gmail.com>
2024-11-12 17:45:16 +0100

    EJERCICIO TERMINADO: hacer una funcion que retorne un ClipRRect y llamarlo para las imagenes

--- Tema2_GeneracionInterfacesUsuario/ejer12_galeria_img/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer12_galeria_img/lib/main.dart
@@ -8,33 +8,76 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ejercicio 12',
+      debugShowCheckedModeBanner: false, // QUITAR MARCA DEBBUG
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const GalleryPage(title: 'Ejercicio 12 Pagina 52'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class GalleryPage extends StatelessWidget {
+  const GalleryPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(
+          "Ejercicio 12 Página 52",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+      body: Center(
+        child: GridView.count(
+          crossAxisCount: 3,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+          padding: EdgeInsets.all(30),
+          // AÑADIR UN PADDING A LAS FOTOS
+
+          children: const <Widget>[
+            PlantillaImagen("gato1.jpg"),
+            PlantillaImagen("frontalPhotoShrek.jpeg"),
+            PlantillaImagen("homer.jpg"),
+            PlantillaImagen("Michis.jpg"),
+            PlantillaImagen("shrekPhoto.jpg"),
+            PlantillaImagen("gato1.jpg"),
+            PlantillaImagen("frontalPhotoShrek.jpeg"),
+            PlantillaImagen("homer.jpg"),
+            PlantillaImagen("Michis.jpg"),
+            PlantillaImagen("shrekPhoto.jpg"),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class PlantillaImagen extends StatelessWidget {
+  final String imageName;
+
+  const PlantillaImagen(this.imageName, {super.key});
+
   @override
   Widget build(BuildContext context) {
-
+    return ClipRRect(
+      // PERMITE AGREGAR UN BORDE REDONDEADO A LA IMAGEN
+      borderRadius: BorderRadius.circular(20),
+      child: Image.asset(
+        'assets/$imageName',
+        // SE PONE BoxFit PARA QUE SE AJUSTE AL ESPACIO DISPONIBLE
+        fit: BoxFit.cover, // NO SE PONE EL TAMAÑO PORQUE EL GRIDVIEW YA LO GESTIONA
+      ),
     );
   }
 }
