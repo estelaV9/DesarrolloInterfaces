e89ef706593b93268c7bb0eee71a9544f55a3bcc
estelaV9 <esteladeveg@gmail.com>
2024-11-13 13:43:34 +0100

    EJERCICIO TERMINADO: modificar un poco el codigo y quitar atributos innecesarios

--- Tema2_GeneracionInterfacesUsuario/ejer15_perfil_user/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer15_perfil_user/lib/main.dart
@@ -42,23 +42,22 @@ class _StatePaginaPantalla extends State<PaginaPantalla> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text(
-          "Ejercicio 15 Pagina 52",
-          style: TextStyle(fontSize: 24),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text(
+            "Ejercicio 15 Pagina 52",
+            style: TextStyle(fontSize: 24),
+          ),
         ),
-      ),
-      body: Center(
-        child: Container(
-          width: 600,
-          height: 600,
-          decoration: BoxDecoration(
-              color: Colors.deepPurpleAccent[100],
-              border: Border.all(color: Colors.black38, width: 5),
-              borderRadius: BorderRadius.circular(20)),
-          child: Padding(
+        body: Center(
+          child: Container(
+            width: 600,
+            height: 600,
             padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent[100],
+                border: Border.all(color: Colors.black38, width: 5),
+                borderRadius: BorderRadius.circular(20)),
             child: Column(children: [
               const Text(
                 "Perfil de Usuario",
@@ -70,56 +69,48 @@ class _StatePaginaPantalla extends State<PaginaPantalla> {
               const Padding(
                 padding: EdgeInsets.all(20),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              Stack(
                 children: [
-                  Stack(
-                    children: [
-                      Positioned(
-                          child: Container(
-                        width: 400.0,
-                        height: 200.0,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular((20)),
-                            // AÑADIMOS AL CONTENEDOR LA IMAGEN DE FONDO ---> AssetImage
-                            image: const DecorationImage(
-                                image: AssetImage("assets/catWallpaper.jpg"),
-                                fit: BoxFit.fill),
-                            border: Border.all(color: Colors.black, width: 2)),
-                      )),
-                      Positioned(
-                          // EL POSITIONED TIENE VALORES PARA PONER MARGIN
-                          top: 37,
-                          left: 30,
-                          child: Row(children: [
-                            CircleAvatar(
-                                radius: 60,
-                                backgroundImage:
-                                    AssetImage("assets/imagenPerfil.jpg"),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      borderRadius:
-                                          BorderRadius.circular((100)),
-                                      // AÑADIMOS AL CONTENEDOR LA IMAGEN DE FONDO ---> AssetImage
-                                      image: const DecorationImage(
-                                          image: AssetImage(
-                                              "assets/imagenPerfil.png"),
-                                          fit: BoxFit.fill),
-                                      border: Border.all(
-                                          color: Colors.lightBlueAccent,
-                                          width: 2)),
-                                )),
-                            const Padding(padding: EdgeInsets.all(14)),
-                            Text(
-                              nombre,
-                              style: const TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white70),
-                            )
-                          ]))
-                    ],
-                  )
+                    Container(
+                    width: 400.0,
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular((20)),
+                        // AÑADIMOS AL CONTENEDOR LA IMAGEN DE FONDO ---> AssetImage
+                        image: const DecorationImage(
+                            image: AssetImage("assets/catWallpaper.jpg"),
+                            fit: BoxFit.fill),
+                        border: Border.all(color: Colors.black, width: 2)),
+                  ),
+                  Positioned(
+                      // EL POSITIONED TIENE VALORES PARA PONER MARGIN
+                      top: 37,
+                      left: 30,
+                      child: Row(children: [
+                        CircleAvatar(
+                            radius: 60,
+                            backgroundImage:
+                                const AssetImage("assets/imagenPerfil.jpg"),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular((100)),
+                                  // AÑADIMOS AL CONTENEDOR LA IMAGEN DE FONDO ---> AssetImage
+                                  image: const DecorationImage(
+                                      image:
+                                          AssetImage("assets/imagenPerfil.png"),
+                                      fit: BoxFit.fill),
+                                  border: Border.all(
+                                      color: Colors.lightBlueAccent, width: 2)),
+                            )),
+                        const Padding(padding: EdgeInsets.all(14)),
+                        Text(
+                          nombre,
+                          style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white70),
+                        )
+                      ]))
                 ],
               ),
               const Padding(padding: EdgeInsets.all(20)),
@@ -158,22 +149,20 @@ class _StatePaginaPantalla extends State<PaginaPantalla> {
                       ]))
             ]),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
 
 class ViewForm extends StatelessWidget {
   final TextEditingController nameController;
 
-  ViewForm({super.key, required this.nameController});
+  const ViewForm({super.key, required this.nameController});
 
   @override
   Widget build(BuildContext context) {
     return Form(
       child: TextFormField(
-        controller: nameController,  // USAMOS EL CONTROLADOR PASANDO COMO PARAMETRO
+        controller: nameController, // USAMOS EL CONTROLADOR PASANDO COMO PARAMETRO
         decoration: const InputDecoration(
           border: OutlineInputBorder(),
           labelText: "Nombre",
