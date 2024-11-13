36a123821a75594af31630cd5f5a0872ddc165a1
Estela de Vega <130656156+estelaV9@users.noreply.github.com>
2024-11-13 18:59:55 +0100

    modificar el ejercicio 7

--- Tema2_GeneracionInterfacesUsuario/ejer7_tarjeta_container/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer7_tarjeta_container/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-/*EJERCICIO 7:  Diseña una tarjeta dentro de un Container con un color
-blanco de fondo, bordes y relleno personalizado. Utiliza las propiedades
-de margin, padding y borderRadius de Container para realizarlo*/
+/* EJERCICIO 7: Diseña una tarjeta dentro de un Container con un color blanco de
+fondo, bordes y relleno personalizado.
+Utiliza las propiedades de margin, padding y borderRadius de Container
+para realizarlo */
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,53 +15,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ejercicio 7',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const PaginaPrincipal(title: 'Ejercicio 7 Pagina 32'),
+      home: const CardPage(title: 'Ejercicio 3'),
     );
   }
 }
 
-class PaginaPrincipal extends StatefulWidget {
-  const PaginaPrincipal({super.key, required this.title});
-
+class CardPage extends StatefulWidget {
   final String title;
 
+  const CardPage({super.key, required this.title});
+
   @override
-  State<PaginaPrincipal> createState() => _TarjetaContainer();
+  State<CardPage> createState() => _CardState();
 }
 
-class _TarjetaContainer extends State<PaginaPrincipal> {
+class _CardState extends State<CardPage> {
+  final TextStyle style = const TextStyle(fontSize: 24);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(
+          widget.title,
+          style: style,
         ),
-        body: Center(
-          child: Container(
-            margin: const EdgeInsets.all(16.0), // ESPACIO EXTERIOR ALREDEDOR DE LA TARJETA
+        centerTitle: true, // CENTRA EL TEXTO DEL APPBAR
+      ),
+      body: Center(
+        child: Container(
+          width: 200.0,
+          // ANCHO DEL CONTENEDOR
+          height: 200.0,
+          // ALTURA DEL CONTENEDOR
+          decoration: BoxDecoration(
             color: Colors.white,
-            child: Container(
-              width: 200.0, // ANCHO DEL CONTENEDOR
-              height: 200.0, // ALTURA DEL CONTENEDOR
-              alignment: Alignment.center,
-               padding: const EdgeInsets.all(25.0), // PADDING PARA EL INTERIOR DEL CONTENEDOR
-              decoration: BoxDecoration(
-                color: Colors.white, // COLOR DE FONDO DEL CONTENEDOR INTERIOR
-                border: Border.all(
-                  color: Colors.purple, // COLOR DEL BORDE
-                  width: 2.0, // ANCHO DEL BORDE
-                ),
-                borderRadius: BorderRadius.circular(10), // RADIO PARA BORDES REDONDEADOS
+            border: Border.all(color: Colors.black38, width: 2),
+            borderRadius: BorderRadius.circular(20),
+            // RADIO PARA BORDES REDONDEADOS
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                offset: Offset(0, 5), // SOMBRA HACIA ABAJO
               ),
-              child: const Text("Texto dentro de un contenedor", style: TextStyle(fontSize: 24),)
+            ],
+          ),
+          padding: const EdgeInsets.all(30),
+          // PADDING PARA EL INTERIOR DEL CONTENEDOR
+          margin: const EdgeInsets.all(50),
+          // ESPACIO EXTERIOR ALREDEDOR DE LA TARJETA
+          child: Center(
+            // CENTRAR EL TEXTO EN EL CONTAINER
+            child: Text(
+              "nfjksdakfds",
+              style: style,
+              textAlign: TextAlign.center,
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
