6adc27b6f54d7c368f3974cd30cfe5408ad91e09
Estela de Vega <130656156+estelaV9@users.noreply.github.com>
2024-11-13 18:05:11 +0100

    mejorar el ejercicio 2

--- Tema2_GeneracionInterfacesUsuario/ejer2_texto_y_boton/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer2_texto_y_boton/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+/*Crea una aplicación que siga una jerarquía simple de widgets utilizando un
+Column que tenga varios Text y un Button (ElevatedButton)
+ • Haz que, al pulsar el botón, muestre un mensaje en la consola.*/
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,46 +11,72 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: "Ejercicio 2",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const TextButtonPage(title: 'Ejercicio 2'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class TextButtonPage extends StatefulWidget {
+  final String title;
+
+  const TextButtonPage({super.key, required this.title});
+
+  @override
+  State<TextButtonPage> createState() => _TextButtonState();
+}
+
+class _TextButtonState extends State<TextButtonPage> {
+  // DEFINIMOS EL ESTILO DEL TEXTO PARA REUTILIZAR
+  final TextStyle textStyle = const TextStyle(fontSize: 24);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Ejercicio 2 Página 31"),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title, style: const TextStyle(fontSize: 24)),
+      ),
+      body: Center(
+        child: Column(
+          // LO CENTRAMOS
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Texto 1",
+              style: textStyle, // PARA APLICAR EL ESTILO TIENE QUE SER UN WIDGET SIN CONST
+            ),
+            const SizedBox(height: 10), // AÑADIR ESPACIO
+            Text(
+              "Texto 2",
+              style: textStyle,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Texto 3",
+              style: textStyle,
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  print("Mensaje por consola"); // CUANDO SE PULSA SE MUESTRA UN MENSAJE POR CONSOLA
+                },
+                child: Text(
+                  "Pulsar Boton",
+                  style: textStyle,
+                ))
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // CENTRA LA COLUMNA
-            children: [
-              Text("Texto 1", style: TextStyle(fontSize: 24)),
-              Text("Texto 2", style: TextStyle(fontSize: 24)),
-
-              // AÑADIR UN MARGEN ENTRE LOS TEXTOS Y EL BOTON DE 20
-              const Padding(padding: EdgeInsets.only(
-                top: 20,
-                bottom: 20,
-              ),),
-
-              ElevatedButton(
-                  onPressed: () {
-                    print("Has pulsado el boton"); // CUANDO SE PRESIONA EL BOTON SE MUESTRA UN MENSAJE POR CONSOLA
-                  },
-                  child: (Text("PULSAR", style: TextStyle(fontSize: 24)) // TEXTO DEL BOTON
-                  )
-              )
-            ],
-          ),
-        )
+      ),
     );
   }
 }
