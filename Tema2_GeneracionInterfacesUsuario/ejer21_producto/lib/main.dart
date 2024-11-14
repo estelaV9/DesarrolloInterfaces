baedb53fd51147240348931d638c975aba1e054d
estelaV9 <esteladeveg@gmail.com>
2024-11-14 09:41:41 +0100

    añadir el container y ponerle estilos

--- Tema2_GeneracionInterfacesUsuario/ejer21_producto/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer21_producto/lib/main.dart
@@ -22,34 +22,53 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Ejercicio 21 Pagina 63'),
+      home: const PantallaPrincipal(title: 'Ejercicio 21 Pagina 63'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class PantallaPrincipal extends StatelessWidget {
   // DEFINIR EL ESTILO PARA LOS TEXTOS DE UN TAMAÑO DE 24
-  TextStyle style = const TextStyle(fontSize: 24);
+  final TextStyle style = const TextStyle(fontSize: 24);
+  const PantallaPrincipal({super.key, required this.title});
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title, style: style),
+        title: Text("Ejercicio 21 Pagina 63", style: style),
         centerTitle: true, // CENTRAR EL APPBAR
       ),
       body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10), // ESPACIO INTERIOR DENTRO DEL CONTAINER
+          margin: const EdgeInsets.symmetric(horizontal: 400, vertical: 50), // ESPACIO EXTERIOR ALREDEDOR DEL CONTAINER
+          decoration: BoxDecoration(
+            color: Colors.deepPurpleAccent.shade100, // COLOR DE FONDO DEL CONTENEDOR
+            borderRadius: BorderRadius.circular(15), // BORDES REDONDEADOS
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3), // COLOR DE LA SOMBRA
+                spreadRadius: 5, // RANGO DE EXPANSIÓN DE LA SOMBRA
+                blurRadius: 10, // INTENSIDAD DEL DESENFOQUE DE LA SOMBRA
+                offset: const Offset(0, 5), // DESPLAZAMIENTO DE LA SOMBRA (X, Y)
+              ),
+            ],
+            border: Border.all(
+              color: Colors.deepPurple, // COLOR DEL BORDE
+              width: 2, // ANCHO DEL BORDE
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
+            ],
+          ),
+        ),
       ),
-    );
+      );
   }
 }
