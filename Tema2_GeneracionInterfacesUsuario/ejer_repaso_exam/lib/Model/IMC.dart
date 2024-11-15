fe1567d53361e6c02326f2dce8db0be704461573
estelaV9 <esteladeveg@gmail.com>
2024-11-15 13:03:16 +0100

    crear dos metodo en la clase imc

--- Tema2_GeneracionInterfacesUsuario/ejer_repaso_exam/lib/Model/IMC.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer_repaso_exam/lib/Model/IMC.dart
@@ -15,4 +15,20 @@ class Imc {
   set kilos(int value) {
     _kilos = value;
   }
+
+  double calcularImc(Imc imc) {
+    return (imc.kilos / (imc.metros * imc.metros));
+  } // CALCULAR EL IMC PASADO
+
+  String informacionImc(double imc) {
+    if (imc < 18.5) {
+      return "Peso inferior al normal";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      return "Peso normal";
+    } else if (imc >= 25 && imc <= 29.9) {
+      return "peso superior al normal";
+    } else {
+      return "sobrepeso";
+    }
+  } // METODO PARA MOSTRAR UN MENSAJE DEPENDIENDO EL IMC CALCULADO
 }
