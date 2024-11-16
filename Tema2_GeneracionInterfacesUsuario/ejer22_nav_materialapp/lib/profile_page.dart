b899136befdae91a0a0000ce5a812964347bb8e6
estelaV9 <esteladeveg@gmail.com>
2024-11-16 16:34:33 +0100

    añadir los botones a las pantallas de perfil y ajuste

--- Tema2_GeneracionInterfacesUsuario/ejer22_nav_materialapp/lib/profile_page.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer22_nav_materialapp/lib/profile_page.dart
@@ -2,20 +2,37 @@ import 'package:flutter/material.dart';
 // PANTALLA DE PERFIL
 class ProfilePage extends StatelessWidget{
   final String title;
-  // DEFINIR EL ESTILO PARA LOS TEXTOS DE UN TAMAÑO DE 24
-  TextStyle style = const TextStyle(fontSize: 24);
-  ProfilePage({super.key, required this.title});
+
+  const ProfilePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle buttonStyle = const TextStyle(fontSize: 24);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title, style: style),
+        title: Text(title),
         centerTitle: true, // CENTRAR EL APPBAR
       ),
       body: Center(
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Home", style: buttonStyle),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/ajustes');
+              },
+              child: Text("Settings", style: buttonStyle),
+            ),
+          ],
+        ),
       ),
     );
   }
--- Tema2_GeneracionInterfacesUsuario/ejer22_nav_materialapp/lib/settings_page.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer22_nav_materialapp/lib/settings_page.dart
@@ -2,20 +2,36 @@ import 'package:flutter/material.dart';
 // PANTALLA DE SETTINGS
 class SettingsPage extends StatelessWidget{
   final String title;
-  // DEFINIR EL ESTILO PARA LOS TEXTOS DE UN TAMAÑO DE 24
-  TextStyle style = const TextStyle(fontSize: 24);
   SettingsPage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle buttonStyle = const TextStyle(fontSize: 24);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title, style: style),
+        title: Text(title),
         centerTitle: true, // CENTRAR EL APPBAR
       ),
       body: Center(
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Home", style: buttonStyle),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/perfil');
+              },
+              child: Text("Profile", style: buttonStyle),
+            ),
+          ],
+        ),
       ),
     );
   }
