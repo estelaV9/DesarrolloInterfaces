43fd1628f2e4a767a75b8038d6d32cf5f68ee70c
estelaV9 <esteladeveg@gmail.com>
2024-11-21 21:36:19 +0100

    crear el ejercicio 28 sobre la navegacion inferior

---
+++ Tema2_GeneracionInterfacesUsuario/ejer28_nav_inferior/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+/*EJERCICIO 28: Crea una aplicación que tenga tres vistas: Home, Perfil y Ajustes. Cada una
+de las pantallas se tiene que poder navegar desde un widget de navegación
+inferior.*/
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
