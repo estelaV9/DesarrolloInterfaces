c39c0bc261b530c3924e5c55cf646ea754b8d8d3
estelaV9 <esteladeveg@gmail.com>
2024-11-21 20:46:21 +0100

    crear ejercicio 18 de una lista de tareas con checkbox

---
+++ Tema2_GeneracionInterfacesUsuario/ejer18_tarea_checkbox/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+/*EJERCICIO 18: Implementa una lista de tareas en la que el usuario pueda marcar las tareas
+como completadas utilizando Checkbox. En caso de que la marque, el texto
+debe cambiar a color gris claro y tacharse.
+ 1. Como extra, que el usuario pueda añadir tareas con algún tipo de Form.*/
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
