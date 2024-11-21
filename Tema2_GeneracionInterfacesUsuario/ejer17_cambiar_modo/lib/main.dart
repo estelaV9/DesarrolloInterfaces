4b52a10761603c54101f1da8f98b44f8a2a5dd1d
estelaV9 <esteladeveg@gmail.com>
2024-11-21 19:19:16 +0100

    crear ejercicio 17 de cambiar entre modo oscuro y claro

---
+++ Tema2_GeneracionInterfacesUsuario/ejer17_cambiar_modo/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+/*EJERCICIO 17: 17.Crea una pantalla de configuración con distintos elementos (textos,
+botones…) un switch que permita cambiar entre modo claro y modo oscuro.
+Al usar el switch, el tema de la aplicación debe cambiar de color.*/
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+      ),
+    );
+  }
+}
