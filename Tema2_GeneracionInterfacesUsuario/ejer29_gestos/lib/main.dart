b94702b6143ed682f2469c2c6cbf14d291970456
estelaV9 <esteladeveg@gmail.com>
2024-11-21 22:00:06 +0100

    crear ejercicio 19 el cual, cuando pulsas un contendor cambia el color

---
+++ Tema2_GeneracionInterfacesUsuario/ejer29_gestos/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+/*EJERCICIO 29: Crea una pantalla que tenga un fondo rojo y un contenedor azul centrado.
+Haz que, cuando pulsemos el contenedor, cambie el color del contenedor y
+el fondo al contrario.*/
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
