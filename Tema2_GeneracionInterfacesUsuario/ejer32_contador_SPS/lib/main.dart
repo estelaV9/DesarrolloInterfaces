651720892c00a43c418475ea8cb6ee3003f7b7f1
estelaV9 <esteladeveg@gmail.com>
2024-11-27 14:01:44 +0100

    crear ejercicio 32 para incrementar un contador con ScopedModel, Provider y SharedPreferences.

---
+++ Tema2_GeneracionInterfacesUsuario/ejer32_contador_SPS/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+/* EJER 32: Crea un programa con un contador siguiendo los ejemplos, que sea
+capaz de mostrar un contador e incrementarlo con un botón flotante
+Impleméntalos usando ScopedModel, Provider y SharedPreferences. ¿Qué
+diferencia hay al volver a abrir la aplicación? */
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+      ),
+    );
+  }
+}
