17bbd41a976825e22e602eec164e3cc6a11735ff
estelaV9 <esteladeveg@gmail.com>
2024-11-27 14:14:34 +0100

    no terminado: cambiar de nombre y poner que retorne un ScopedModelDescendant

--- Tema2_GeneracionInterfacesUsuario/ejer32_contador_SPS/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer32_contador_SPS/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 /* EJER 32: Crea un programa con un contador siguiendo los ejemplos, que sea
 capaz de mostrar un contador e incrementarlo con un botón flotante
@@ -6,34 +7,43 @@ Impleméntalos usando ScopedModel, Provider y SharedPreferences. ¿Qué
 diferencia hay al volver a abrir la aplicación? */
 
 void main() {
-  runApp(const MyApp());
+  runApp(ContadorDisplay());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ContadorDisplay extends StatelessWidget {
+  ContadorDisplay({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return ScopedModelDescendant<ContadorModel>(
+      builder: (context, child, model) {
+        return Center(
+          child: Text('Contador: ${model.contador}',
+              style: TextStyle(fontSize: 24)),
+        );
+      },
+    );
+    /*return MaterialApp(
+      title: 'EJERCICIO 32',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      home: const MyHomePage(title: 'Ejercicio 32. Contador'),
+    );*/
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,9 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-
-      ),
+      body: Center(),
     );
   }
 }
