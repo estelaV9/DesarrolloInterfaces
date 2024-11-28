07b62259a31dd67be140c7ee4943ef443864a19c
estelaV9 <esteladeveg@gmail.com>
2024-11-28 20:20:19 +0100

    EJERCICIO TERMINADO: hacer que se guarde la lista y mostrarla

--- Tema2_GeneracionInterfacesUsuario/ejer33_gestion_tareas/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer33_gestion_tareas/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'add_task.dart';
 
 /* EJERCICIO 33: Crea una aplicación para gestionar tareas usando Provider,
 que cumpla las siguientes características:
@@ -16,7 +18,12 @@ que cumpla las siguientes características:
 
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TaskProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -26,36 +33,72 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ejercicio 33',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Ejercicio 33 Pagina 93'),
+      home: const ListTasks(title: 'Ejercicio 33 Pagina 93'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// NO CAMBIA EL ESTADO, SE RECONSTRUYE -> SE USA StatelessWidget
+class ListTasks extends StatelessWidget {
+  const ListTasks({super.key, required this.title});
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
+        child: Column(
+          children: [
+            const Text("Lista de Tareas", style: TextStyle(fontSize: 24)),
 
+            // MOSTRAR LA LISTA DE TODAS LAS TAREAS
+            Expanded(
+              child: Consumer<TaskProvider>(
+                builder: (context, taskProvider, child) {
+                  return ListView.builder(
+                    itemCount: taskProvider.listNames.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(taskProvider.listNames[index]),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddTaskScreen()),
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
+  }
+}
+
+class TaskProvider extends ChangeNotifier {
+  final List<String> _listNames = []; // LSITA TAREAS
+
+  List<String> get listNames => _listNames; // GET TAREAS
+
+  void addTask(String newTask) {
+    _listNames.add(newTask); // AGREGAR LA NUEVA TAREA A LA LISTA
+    notifyListeners(); // NOTIFICAR A LOS LISTENERS QUE LA LISTA CAMBIO
   }
 }
