516258217b8fd632c7bbf5eae96e7dffa5ff6c9c
estelaV9 <esteladeveg@gmail.com>
2024-11-28 17:22:06 +0100

    crear la clase scoped_model, shared_preferenced y provider

---
+++ Tema2_GeneracionInterfacesUsuario/ejer32_contador_SPS/lib/screen/shared_preferenced.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/* PANTALLA DE EJEMPLO PARA GUARDAR Y OBTENER DATOS USANDO SHARED PREFERENCES */
+class SharedPreferencesScreen extends StatefulWidget {
+  const SharedPreferencesScreen({super.key});
+
+  @override
+  State<SharedPreferencesScreen> createState() => _SharedPreferencesScreenState();
+}
+
+class _SharedPreferencesScreenState extends State<SharedPreferencesScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
