cb14bfec573c2de578dd432a999df855317952d8
estelaV9 <esteladeveg@gmail.com>
2024-12-04 12:51:28 +0100

    crear un ejercicio sencillo para trabajar con SQLite

---
+++ Tema2_GeneracionInterfacesUsuario/ejer_sencillo_sqlite/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+/* CREAR UNA BASE DE DATOS (id, nombre, edad) Y MOSTRAR UNA LISTA CON LOS NOMBRES
+* TENDRA TRES BOTONES QUE SEAN READ (carga a la lista), UPDATE (jose -> 30) Y
+* DELETE DONDE ELIMINARA A JOSE*/
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ejercicio sencillo SQLite',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Ejercicio sencillo SQLite'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+      ),
+    );
+  }
+}
