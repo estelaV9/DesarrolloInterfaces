0a035b489a2e0dfa132430b209f3f9e68fb05eb6
estelaV9 <esteladeveg@gmail.com>
2024-12-04 12:59:36 +0100

    añadir la base de datos

--- Tema2_GeneracionInterfacesUsuario/ejer_sencillo_sqlite/lib/main.dart
+++ Tema2_GeneracionInterfacesUsuario/ejer_sencillo_sqlite/lib/main.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:path/path.dart';
 
 /* CREAR UNA BASE DE DATOS (id, nombre, edad) Y MOSTRAR UNA LISTA CON LOS NOMBRES
 * TENDRA TRES BOTONES QUE SEAN READ (carga a la lista), UPDATE (jose -> 30) Y
 * DELETE DONDE ELIMINARA A JOSE*/
 
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // INICIALIZAR EL sqflite PARA ESCRITORIO
+  sqfliteFfiInit();
+  final databaseFactory = databaseFactoryFfi;
+
+  // OBTENER LA RUTA DE LA BASE DE DATOS
+  final dbPath = join(await databaseFactory.getDatabasesPath(), 'persons.db');
+  final database = await databaseFactory.openDatabase(dbPath);
+
+  // CREA LA TABLA 'person' SI NO EXISTE
+  await database.execute('''
+ CREATE TABLE IF NOT EXISTS person (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,  
+   nombre TEXT NOT NULL,                   
+   edad TEXT NOT NULL 
+ )
+ ''');
+
+  // INICIA LA APLICACIÓN Y PASA LA BASE DE DATOS A LA INTERFAZ PRINCIPAL
+  runApp(PersonApp(database: database));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PersonApp extends StatelessWidget {
+  final Database database;
+
+  const PersonApp({super.key, required this.database});
 
   @override
   Widget build(BuildContext context) {
