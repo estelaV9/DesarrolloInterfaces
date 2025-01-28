b7c569e4ee07920bf8c1262c8fbd17d1f3f0651f
estelaV9 <esteladeveg@gmail.com>
2025-01-28 20:28:44 +0100

    EJERCICIO TERMINADO: crear una lista y aplicar el PageStorage. Crear la apk para probarla

--- Tema3_FuncionesAvanzadas/3.2/ejer2_rotar_pantalla/lib/main.dart
+++ Tema3_FuncionesAvanzadas/3.2/ejer2_rotar_pantalla/lib/main.dart
@@ -9,10 +9,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MyList(),
+    );
+  }
+}
+
+class MyList extends StatefulWidget {
+  const MyList({super.key});
+
+  @override
+  _MyListState createState() => _MyListState();
+}
+
+class _MyListState extends State<MyList> {
+  final List<String> items = List.generate(50, (index) => 'Elemento $index');
+
+  // KEY PARA GUARDAR EL ESTADO DE LA POSICION DEL DESPLAZAMIENTO
+  final PageStorageKey _key = const PageStorageKey('myListKey');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista Persistente'),
+      ),
+      body: PageStorage(
+        bucket: PageStorageBucket(),
+        child: ListView.builder(
+          key: _key, // ASOCIA EL KEY A LA LISTA PARA GUARDAR EL ESTADO
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(items[index]),
+            );
+          },
         ),
       ),
     );
