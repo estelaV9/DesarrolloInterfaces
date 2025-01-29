616189b5df28bd22f348677771e16490c56735f0
estelaV9 <esteladeveg@gmail.com>
2025-01-29 22:19:54 +0100

    añadir los archivos y dependencias para realizar el cambio de idiomas

--- Examen_2Evaluacion/repaso_examen/lib/main.dart
+++ Examen_2Evaluacion/repaso_examen/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:repaso_examen/view/converter.dart';
+import 'package:repaso_examen/view/settings.dart';
+import 'package:repaso_examen/view_model/change_theme_provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ChangeThemeProvider(),
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +20,25 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    // OBTENEMOS EL TEMA ACTUAL
+    final themeData = context.watch<ChangeThemeProvider>().themeData;
+    return MaterialApp(
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate
+        ],
+        supportedLocales: [Locale('es'), Locale('en')],
+        locale: Locale('en'),
+        theme: themeData,
+        routes: {
+          //'/': (context) => MainPage(),
+          '/settings': (context) => Settings(),
+          '/converter': (context) => ConverterPage(),
+          //'/transactions': (context) => Transactions(),
+        },
+        debugShowCheckedModeBanner: false,
+        home: Settings());
   }
 }
